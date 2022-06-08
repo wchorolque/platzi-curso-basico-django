79ddfbcd73f8b15a14a5cee83ddfc0abe0f2d14e
williams chorolque <wchorolque@gmail.com>
2022-06-08 13:28:47 -0400

    Completado 12/29: La consola interactiva de Django

--- src/polls/models.py
+++ src/polls/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_data = models.DateField("date published")
+    pub_date = models.DateField("date published")
 
 
-class Choices(models.Model):
+class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
