224756d4d5fd30e3291c33919c1233996194d9ee
williams chorolque <wchorolque@gmail.com>
2022-06-11 14:33:56 -0400

    Completado 21/29: Creando el template home

--- src/polls/views.py
+++ src/polls/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-    return HttpResponse('Estás en la página principal de Premios Platzi App')
+    latest_question_list = Question.objects.all()
+    
+    return render(request, "polls/index.html", {
+        'latest_question_list': latest_question_list
+        })
 
 
 def detail(request, question_id):
