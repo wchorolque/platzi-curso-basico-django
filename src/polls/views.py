95d5fe8d7b3a596bd42d0b7d1138b7d2c7ec5f5e
williams chorolque <wchorolque@gmail.com>
2022-06-12 04:27:19 -0400

    Completado 26/29: Creando la vista results

--- src/polls/views.py
+++ src/polls/views.py
@@ -9,21 +9,19 @@ from .models import Question
 def index(request):
     latest_question_list = Question.objects.all()
 
-    return render(request, "polls/index.html", {
-        'latest_question_list': latest_question_list
-        })
+    return render(request, "polls/index.html", { 'latest_question_list': latest_question_list })
 
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
 
-    return render(request, "polls/detail.html", {
-        'question': question
-    })
+    return render(request, "polls/detail.html", { 'question': question })
 
 
 def results(request, question_id):
-    return HttpResponse(f"Estás los resultados de la pregunta numero {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+
+    return render(request, "polls/results.html", { 'question': question })
 
 
 def vote(request, question_id):
