efac47c1146c0bc939fa923222fc5293a7eb9d4f
williams chorolque <wchorolque@gmail.com>
2022-06-12 13:02:26 -0400

    Completado 28/29: Implementando generic views en la aplicación

--- src/polls/urls.py
+++ src/polls/urls.py
@@ -4,12 +4,8 @@ from . import views
 app_name = 'polls'
 
 urlpatterns = [
-    # ex: /polls/5/
-    path('', views.index, name="index"),
-    # ex: /polls/5/
-    path("<int:question_id>/", views.detail, name="detail"),
-    # ex: /polls/5/results
-    path("<int:question_id>/results/", views.results, name="results"),
-    # ex: /polls/5/vote
-    path("<int:question_id>/vote/", views.vote, name="vote"),
+    path('', views.IndexView.as_view(), name="index"), # ex: /polls/5/
+    path("<int:pk>/", views.QuestionDetailView.as_view(), name="detail"), # ex: /polls/5/
+    path("<int:pk>/results/", views.ResultDetailView.as_view(), name="results"), # ex: /polls/5/results
+    path("<int:question_id>/vote/", views.vote, name="vote"), # ex: /polls/5/vote
 ]
