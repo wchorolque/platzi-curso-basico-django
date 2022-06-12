82539fce1a1844c98de85e96ce137b4f7eb40787
williams chorolque <wchorolque@gmail.com>
2022-06-12 03:06:45 -0400

    Completado 22/29: Elevando el error 404

--- src/polls/urls.py
+++ src/polls/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     # ex: /polls/5/
     path('', views.index, name="index"),
     # ex: /polls/5/
-    path("<int:question_id>/", views.detail, name="polls_detail"),
+    path("<int:question_id>/", views.details, name="polls_detail"),
     # ex: /polls/5/results
     path("<int:question_id>/results/", views.results, name="polls_results"),
     # ex: /polls/5/vote
--- src/polls/views.py
+++ src/polls/views.py
@@ -1,17 +1,22 @@
+from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.all()
-    
+
     return render(request, "polls/index.html", {
         'latest_question_list': latest_question_list
         })
 
 
-def detail(request, question_id):
-    return HttpResponse(f"Estás viendo la pregunta numero {question_id}")
+def details(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+
+    return render(request, "polls/details.html", {
+        'question': question
+    })
 
 
 def results(request, question_id):
